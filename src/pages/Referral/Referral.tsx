d8dcd0587d4cbec6722e10d2dfe62e557b55ff6e
jymin99 <min10909@naver.com>
2024-11-07 16:19:30 +0900

    fix: share to friend 순서 바꾸기 오류 수정

--- src/pages/Referral/Referral.tsx
+++ src/pages/Referral/Referral.tsx
@@ -36,7 +36,7 @@ interface ModalState {
 
 const ShareToFriend = ({ link ,text}) => {
   const shareToTelegram = () => {
-    const telegramLink = `text=${encodeURIComponent(text)}&https://t.me/share/url?url=${encodeURIComponent(link)}`;
+    const telegramLink = `https://t.me/share/url?text=${encodeURIComponent(text)}&url=${encodeURIComponent(link)}`;
     window.open(telegramLink, '_blank');
   };
 
